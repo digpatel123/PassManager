c14025f37850bc2b71d2636d7a7e0c168485ed54
digpatel123 <59032434+digpatel123@users.noreply.github.com>
2023-08-30 20:02:33 +0100

    Added search functionality and changed the output to a json file format.

--- main.py
+++ main.py
@@ -1,17 +1,31 @@
 from tkinter import *
 import random
 import string
+import json
 from tkinter import messagebox
-import pyperclip
 
 def save_data(website, email, password):
-    with open("data.txt", "a") as file:
-        file.write(f"Website: {website}\n")
-        file.write(f"Email/Username: {email}\n")
-        file.write(f"Password: {password}\n\n")
+    data = {
+        website: {
+            "email": email,
+            "password": password
+        }
+    }
+    with open("data.json", "a") as file:
+        json.dump(data, file)
+        file.write("\n")
+
+def search_data(website):
+    with open("data.json", "r") as file:
+        for line in file:
+            data = json.loads(line)
+            if website in data:
+                messagebox.showinfo("Search Result", f"Website: {website}\nEmail/Username: {data[website]['email']}\nPassword: {data[website]['password']}")
+                return
+        messagebox.showinfo("Search Result", "Website not found.")
 
 def generate_password():
-    password_length = random.randint(8,13)
+    password_length = 8
     password_characters = string.ascii_letters + string.digits + string.punctuation
     password = ''.join(random.choice(password_characters) for _ in range(password_length))
     return password
@@ -24,7 +38,7 @@ def create_form():
     canvas = Canvas(window, height=200, width=200)
     logo = PhotoImage(file="logo.png")
     canvas.create_image(100, 100, image=logo)
-    canvas.grid(row=0, column=0, columnspan=3)
+    canvas.grid(row=0, column=0, columnspan=4)
 
     # Creating labels
     website_label = Label(text="Website:")
@@ -36,10 +50,9 @@ def create_form():
 
     # Creating Textboxes
     website_entry = Entry(width=35)
-    website_entry.grid(row=1, column=1, columnspan=2, sticky="we")
+    website_entry.grid(row=1, column=1, sticky="e")
     email_entry = Entry(width=35)
     email_entry.grid(row=2, column=1, columnspan=2, sticky="we")
-    email_entry.insert(0, "dj@dj.com")
     pass_entry = Entry(width=21)
     pass_entry.grid(row=3, column=1, sticky="w")
 
@@ -63,13 +76,22 @@ def create_form():
         password = generate_password()
         pass_entry.delete(0, END)
         pass_entry.insert(0, password)
-        pyperclip.copy(password)
+
+    def search_website():
+        website = website_entry.get()
+        if website == "":
+            messagebox.showerror("Error", "Please enter a website name to search.")
+        else:
+            search_data(website)
 
     # Creating buttons
     gen_pass_button = Button(text="Generate password", command=generate_and_insert_password)
     gen_pass_button.grid(row=3, column=2, sticky="e")
-    add_button = Button(text="Add", width=36, command=add_data)
-    add_button.grid(row=4, column=1, columnspan=2)
+    add_button = Button(text="Add", width=10, command=add_data)
+    add_button.grid(row=4, column=1, sticky="e")
+
+    search_button = Button(text="Search", width=10, command=search_website)
+    search_button.grid(row=1, column=2, sticky="e")
 
     window.mainloop()
 
